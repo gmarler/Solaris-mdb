21e96e58742569022a4bc9210d3e1d6be7a1b788
Gordon Marler <gmarler@gmarler.com>
2015-03-03 08:14:53 -0500

    initial test, Expect::Simple spike

--- t/lib/TF/Solaris/mdb.pm
+++ t/lib/TF/Solaris/mdb.pm
@@ -4,4 +4,18 @@ package TF::Solaris::mdb;
 use Test::Class::Moose;
 with 'Test::Class::Moose::Role::AutoUse';
 
+sub test_startup {
+  my $test = shift;
 
+  unless ($^O eq 'Win32') {
+    $test->test_skip("Tests won't run on $^O");
+  }
+}
+
+sub test_load {
+  my $test = shift;
+
+  diag "test load";
+}
+
+1;
