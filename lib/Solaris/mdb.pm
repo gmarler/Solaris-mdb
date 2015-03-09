418f45bfd3bf3914dae07c8ac01d5cc0f3e93d5a
Gordon Marler <gmarler@gmarler.com>
2015-03-08 20:11:50 -0400

    Add mdb attribute

--- lib/Solaris/mdb.pm
+++ lib/Solaris/mdb.pm
@@ -4,12 +4,31 @@ use warnings;
 package Solaris::mdb;
 
 # VERSION
-# ABSTRACT: Provides a Perl interface to the Solaris mdb utility
+# ABSTRACT: Provides a Perl interface to the Solaris mdb utility in kernel mode
 #
 use namespace::autoclean;
 use Expect;
 use Moose;
 
+# Expect instance that's a connection to an mdb session
+has mdb => ( isa          => Expect,
+             is           => ro,
+             lazy_builder => 1,
+             predicate    => 'has_mdb',
+             clearer      => 'clear_mdb',
+           );
 
+sub _build_mdb {
+  my ($self) = shift;
+
+  my $e = Expect->new();
+  $e->raw_pty(1);
+  $e->spawn('mdb', '-k') or die "cannot spawn mdb -k: $!\n";
+  return $e;
+}
+
+sub variable_exists {
+
+}
 
 1;
