09579be695a832eedc19897e512e63f5feadcb15
Gordon Marler <gmarler@gmarler.com>
2015-03-11 00:16:38 -0400

    Begin work on Solaris::mdb::Variable

--- lib/Solaris/mdb/Variable.pm
+++ lib/Solaris/mdb/Variable.pm
@@ -3,4 +3,23 @@ use warnings;
 
 package Solaris::mdb::Variable;
 
+# VERSION
+# ABSTRACT: Provides a Perl interface to Solaris mdb variable instances
+#
+use namespace::autoclean;
+use Expect;
+use Moose;
+
+has size_bytes => ( isa        => 'Int',
+                    is         => 'ro',
+                  # required   => 1,
+                  );
+
+has name       => ( isa        => 'Str',
+                    is         => 'ro',
+                    required   => 1,
+                  );
+
+
+
 1;
