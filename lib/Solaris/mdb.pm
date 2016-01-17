9b91ea86533cab8a1eb33fda19d6e1dd5a7882f5
Gordon Marler <gmarler@gmarler.com>
2016-01-17 01:03:15 -0500

    Add better Expect handling - tests pass

--- lib/Solaris/mdb.pm
+++ lib/Solaris/mdb.pm
@@ -6,45 +6,280 @@ package Solaris::mdb;
 
 # VERSION
 # ABSTRACT: Provides a Perl interface to the Solaris mdb utility in kernel mode
-#
-use namespace::autoclean;
-use Expect;
+
+=head1 NAME
+
+Solaris::mdb - A Perl Interface to the Solaris mdb command
+
+=cut
+
 use Moose;
+use Moose::Util::TypeConstraints;
+with 'MooseX::Log::Log4perl';
+use namespace::autoclean;
+use Log::Log4perl qw(:easy);
+use Expect        qw(exp_continue);
+#use Try::Tiny;
+#use Throwable::Factory
+#  GeneralException => undef;
 
-# Expect instance that's a connection to an mdb session
-has mdb => ( isa        => 'Expect',
-             is         => 'ro',
-             lazy_build => 1,
-             predicate  => 'has_mdb',
-             clearer    => 'clear_mdb',
-           );
+has [ 'expect' ]   =>   ( is => 'ro', isa => 'Expect',
+                          builder => '_build_expect', lazy_build => 1, );
+has [ 'mdb_bin' ]  =>   ( is => 'ro', isa => 'Str', default => '/usr/bin/mdb' );
+has [ 'timeout' ]  =>   ( is => 'ro', isa => 'Int', default => 5 );
 
-sub _build_mdb {
-  my ($self) = shift;
+sub _build_expect {
+  my $self = shift;
+  my $log = $self->logger;
 
-  my $e = Expect->new();
-  $e->raw_pty(1);
-  $e->log_stdout(0);
-  $e->spawn('mdb', '-k') or die "cannot spawn mdb -k: $!\n";
+  # mdb might not like your default TERM, so we set it to something we know it's
+  # ok with here
+  $ENV{'TERM'} = "vt100";
 
-  # Even if we don't have the necessary privilege,
-  # which is file_dac_read on /devices/pseudo/mm@0:kmem
-  # Expect will still be successful.  So we need to 'expect()' on the following
-  # output in such cases:
+  my $exp  =  Expect->new;
+  # Needs a raw pseudo-tty
+  $exp->raw_pty(1);
+  $exp->log_stdout(0);
+  # TODO: Make this an option to the constructor
+  #$exp->debug(1);
+  $exp->spawn("/usr/bin/pfexec", $self->mdb_bin, "-k")
+    or die("Cannot spawn [/usr/bin/pfexec " . $self->mdb_bin . "]: $!");
 
-  #  mdb: cannot open /dev/kmem
-  #  mdb: failed to initialize target: Permission denied
-  #$e->expect( 2,
-  #           # [ qr/^mdb:\s+failed\s+[^\n]+Permission\sdenied/s,
-  #           [ qr/Permission\s+denied/s,
-  #             sub { die "No permission to invoke mdb -k: " . $e->before(); return; }
-  #           ],
-  #           #[ eof => sub { die "crapped out"; } ],
-  #          );
+  # See if process immediately exited with error message due to no having
+  # proper privileges:
+  $exp->expect($self->timeout,
+    [ qr{mdb:\sfailed\sto\sopen\s/dev/kmem:\sPermission\sdenied},
+                        sub {
+                          my $self = shift;
+                          my $str = $self->match();
+                          $log->logdie( "Insufficient Privileges" );
+                        } ],
+  );
 
-  return $e;
+  #
+  # mdb: failed to open /dev/kmem: Permission denied
+  #
+
+  $self->logger->debug( "Expect object built" );
+
+  return $exp;
 }
 
+=method DEMOLISH
+
+Standard Moose destructor, needed to cleanly close the Expect object's
+connection to the mdb command.
+
+=cut
+
+sub DEMOLISH {
+  my $self = shift;
+
+  # Only try soft close if the spawned PID is still alive
+  if ($self->expect->pid) {
+    $self->expect->soft_close();
+  }
+}
+
+
+=method quit
+
+Quit/Exit the mdb process this object is connected to
+
+=cut
+
+sub quit {
+  my $self = shift;
+  my $exp_obj = $self->expect;
+  my $log = $self->logger;
+  my $str;
+
+  $log->debug("Quitting mdb");
+
+  # The clean way
+  $exp_obj->expect($self->timeout,
+    [ qr/\r?\>\s/,  sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $self->send("\$q\n");
+                          exp_continue;
+                        } ],
+    [ 'eof',        sub { $log->debug("Encountered EOF");
+                        } ],
+    [ 'timeout',    sub { $log->logdie("TIMEOUT, match failed");
+                        } ],
+  );
+
+  # Just in case we failed miserably above
+  $exp_obj->soft_close();
+
+  my $exit_status = $exp_obj->exitstatus();
+
+  if (defined($exit_status)) {
+    $exit_status >>= 8;
+    $log->debug("Exited with status $exit_status");
+  } else {
+    $log->logdie("Could not extract exit status");
+  }
+
+  return 1;
+}
+
+=method capture_dcmd
+
+Return the output of a given mdb dcmd
+
+=cut
+
+sub capture_dcmd {
+  my $self    = shift;
+  my $dcmd    = shift;
+  my $log     = $self->logger;
+  my $exp_obj = $self->expect;
+  my ($str,$retval,$output);
+
+  $log->debug("Running dcmd [$dcmd]");
+
+  $exp_obj->expect($self->timeout,
+    #
+    # Valid dcmd will return possibly multiline output, followed by a prompt
+    #
+    [ qr/Total[^\n]+\n/,
+                    sub { my $self = shift;
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [" . $self->match() . "]");
+                          $log->debug("AFTER: [" . $self->after() . "]");
+                          $str  = $self->before() . $self->match();
+                          # Strip the dcmd + newline off the beginning of the
+                          # output
+                          ($output = $str) =~ s/^\Q${dcmd}\E\n//smx;
+                          $log->debug("USEFUL OUTPUT: [$output]");
+                          if ($output) { $retval = $output; }
+                          else         { $retval =   undef; }
+                        } ],
+    # invalid dcmd
+    [ qr/mdb:\sinvalid\scommand\s'${dcmd}':\sunknown\sdcmd\sname/,
+                    sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $log->warn("${dcmd} appears to be a non-existent dcmd");
+                          $retval = undef; # FALSE/FAILED/DOESN'T EXIST
+                        } ],
+    [ qr/\r?\>\s/,  sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $self->send("${dcmd}\n");
+                          exp_continue;
+                        } ],
+    [ 'eof',        sub { $log->debug("Encountered EOF");
+                        } ],
+    [ 'timeout',    sub { $log->logdie("TIMEOUT, match failed");
+                        } ],
+  );
+
+  return $retval;
+}
+
+=method kvar_exists
+
+Return true if specified kernel variable exists, false (0), otherwise
+
+=cut
+
+sub kvar_exists {
+  my $self = shift;
+  my $kvar = shift;
+  my $exp_obj = $self->expect;
+  my $log = $self->logger;
+  my ($str, $retval);
+
+  $log->debug("Checking whether kernel variable [$kvar] exists in this kernel");
+
+  $exp_obj->expect($self->timeout,
+    # invalid kernel variable
+    [ qr/mdb:\sfailed\sto\sdereference\ssymbol:\sunknown\ssymbol\sname/,
+                    sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $retval = 0; # FALSE/FAILED/DOESN'T EXIST
+                        } ],
+    # Valid kernel variable will return a valid numeric size > 0
+    # TODO: Validate the captured match value
+    [ qr/\r?\d+/,   sub { $retval = 1; } ],
+    [ qr/\r?\>\s/,  sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $self->send("${kvar}::nm -f sz -dh\n");
+                          exp_continue;
+                        } ],
+    [ 'eof',        sub { $log->debug("Encountered EOF");
+                        } ],
+    [ 'timeout',    sub { $log->logdie("TIMEOUT, match failed");
+                        } ],
+  );
+
+  return $retval;
+}
+
+=method kvar_size
+
+Returns size (in bytes) of specified kernel variable.
+
+Returns 0 in case the output couldn't be extracted
+
+Returns undef if the variable doesn't exist.
+
+=cut
+
+sub kvar_size {
+  my $self = shift;
+  my $kvar = shift;
+  my $exp_obj = $self->expect;
+  my $log = $self->logger;
+  my ($str, $retval, $sz);
+
+  $log->debug("Checking size (in bytes) of kernel variable [$kvar]");
+
+  $exp_obj->expect($self->timeout,
+    # invalid kernel variable
+    [ qr/mdb:\sfailed\sto\sdereference\ssymbol:\sunknown\ssymbol\sname/,
+                    sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $retval = undef; # FALSE/FAILED/DOESN'T EXIST
+                        } ],
+    # Valid kernel variable will return a valid numeric size > 0
+    # TODO: Extract the actual value and verify it's a decimal number
+    [ qr/\r?\d+/,   sub { my $self = shift;
+                          $str  = $self->match();
+                          ($sz) = $str =~ m{(\d+)};
+                          if ($sz) { $retval = $sz; }
+                          else     { $retval =   0; }
+                        } ],
+    [ qr/\r?\>\s/,  sub { my $self = shift;
+                          $str = $self->match();
+                          $log->debug("BEFORE: [" . $self->before() . "]");
+                          $log->debug("MATCHED: [$str]");
+                          $self->send("${kvar}::nm -f sz -dh\n");
+                          exp_continue;
+                        } ],
+    [ 'eof',        sub { $log->debug("Encountered EOF");
+                        } ],
+    [ 'timeout',    sub { $log->logdie("TIMEOUT, match failed");
+                        } ],
+  );
+
+  return $retval;
+}
+
+
+#
 # $e->expect(2,
 #   [ qr/^>\s(?!\s+)/m => sub { my $e = shift;
 #                              say "PROMPT matched";
@@ -79,30 +314,6 @@ sub _build_mdb {
 #   ],
 # );
 
-
-sub DEMOLISH {
-  my ($self) = shift;
-
-  # Refactor
-  my $e = $self->mdb;
-  # say "In DEMOLISH";
-  my $pid = $e->pid();
-  if (defined($pid)) {
-    # say "mdb PID: " . $pid; 
-    # say "sending mdb quit";
-    $e->send('$q' . "\n");
-    $e->expect(undef);
-    # say $e->before();
-    my $status = $e->exitstatus();
-    # if ($status == 0) {
-    #   say "mdb exited cleanly";
-    # } else {
-    #   say "mdb exited BADLY: $status";
-    # }
-  } else {
-    # say "mdb already dead";
-  }
-}
 
 sub variable_exists {
   my ($self) = shift;
@@ -161,4 +372,9 @@ sub variable_exists {
   return $exists;
 }
 
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
 1;
+
+
--- t/lib/TF/Solaris/mdb.pm
+++ t/lib/TF/Solaris/mdb.pm
@@ -4,8 +4,30 @@ package TF::Solaris::mdb;
 use Test::Class::Moose;
 with 'Test::Class::Moose::Role::AutoUse';
 
+# Test mdb object we'll be passing around
+has 'test_mdb' => ( is => 'rw', isa => 'Solaris::mdb' );
+
 sub test_startup {
   my $test = shift;
+  $test->next::method;
+
+  # Log::Log4perl Configuration in a string ...
+  my $conf = q(
+    #log4perl.rootLogger          = DEBUG, Logfile, Screen
+    log4perl.rootLogger          = DEBUG, Screen
+
+    #log4perl.appender.Logfile          = Log::Log4perl::Appender::File
+    #log4perl.appender.Logfile.filename = test.log
+    #log4perl.appender.Logfile.layout   = Log::Log4perl::Layout::PatternLayout
+    #log4perl.appender.Logfile.layout.ConversionPattern = [%r] %F %L %m%n
+
+    log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+    log4perl.appender.Screen.stderr  = 0
+    log4perl.appender.Screen.layout = Log::Log4perl::Layout::SimpleLayout
+  );
+
+  # ... passed as a reference to init()
+  Log::Log4perl::init( \$conf );
 
   unless ($^O eq 'solaris') {
     $test->test_skip("Tests won't run on $^O");
@@ -16,6 +38,18 @@ sub test_startup {
   unless ($status == 0) {
     $test->test_skip("Cannot run tests without proper privilege for mdb -k");
   }
+
+  # Extract live data tests if LIVE_TEST_DATA env variable exists and is set to a
+  # 'truthy' value
+  #if ( exists($ENV{'LIVE_TEST_DATA'}) and $ENV{'LIVE_TEST_DATA'} ) {
+  #  # TODO: Only proceed if we're running on Solaris 11 or later
+  #  diag "LIVE_TEST_DATA is set: testing with live data";
+  #} else {
+  #  diag "Testing with canned data";
+  #  diag "If you want to test with live data, set envvar LIVE_TEST_DATA=1";
+  #}
+  $test->test_mdb( $test->class_name->new() );
+
 
 }
 
@@ -43,49 +77,175 @@ sub test_without_priv {
   #       object for mdb
 }
 
-sub test_expect_meta {
-  my $test = shift;
+sub test_constructor {
+  my ($test) = shift;
 
-  my $mdb = Solaris::mdb->new();
+  ok my $mdb = $test->test_mdb, 'We should have a test object';
 
-  my $e = $mdb->mdb;
-  isa_ok($e, 'Expect', 'Should be Expect object');
-
-  cmp_ok($e->log_stdout(), '==', 0, 'Logging to STDOUT disabled');
-  cmp_ok($e->raw_pty(), '==', 1, 'Raw pty enabled');
+  isa_ok($mdb, $test->class_name);
+#  my $mdb = new_ok("Solaris::mdb" => [],
+#                   "Object constructed correctly");
 }
 
-sub test_expect_mdb_exits_ok {
-  my $test = shift;
+sub test_expect {
+  my ($test) = @_;
 
-  my $mdb = Solaris::mdb->new();
-  my $e = $mdb->mdb;
-  isa_ok($e, 'Expect', 'Should be Expect object');
+  my $mdb = $test->test_mdb;
+  #my $mdb = new_ok("Solaris::mdb" => [ ],
+  #                 "Object constructed correctly");
 
-  cmp_ok($e->pid(), '>', 0, "PID is real");
+  isa_ok $mdb->expect, 'Expect',
+    'lazy builder for Expect object works as expected';
 
-  # Wait for one second, for any output whatsoever
-  $e->expect(1);
-  # Now check to see if mdb has emitted its famous init message
-  like($e->before(), qr/Loading\s+modules:/m, 'Loading modules message');
+  isnt($mdb->expect->pid, undef, 'There is an mdb PID');
 
-  #$e->hard_close();
-  #cmp_ok($e->exitstatus(), '==', 1, 'hard close exit status 1');
+  can_ok($mdb, 'quit');
+
+  cmp_ok($mdb->expect->log_stdout(), '==', 0, 'Logging to STDOUT disabled');
+  cmp_ok($mdb->expect->raw_pty(), '==', 1, 'Raw pty enabled');
+
+  TODO: {
+    local $TODO = "Not done figuring out Try::Tiny exceptions";
+#
+   my $bad_mdb = new_ok("Solaris::mdb" => [ "mdb_bin", "/usr/bin/junk_mdb" ],
+     "Create object with bad mdb_bin");
+
+   #dies_ok { $bad_mdb->expect } 'expect to die while trying to build Expect object';
+
+   #throws_ok( sub { $bad_mdb->expect },
+   #           "Cannot execute mdb_bin",
+   #           'expect to die while trying to build Expect object' );
+
+    undef $bad_mdb;
+  }
 }
 
-sub test_variable_exists {
+sub test_quit {
   my $test = shift;
 
-  my $mdb = Solaris::mdb->new();
+  my $mdb = $test->test_mdb;
+  #my $mdb = new_ok("Solaris::mdb" => [ ],
+  #                 "Object constructed correctly");
 
-  throws_ok { $mdb->variable_exists() } qr/requires\sa\svariable/,
-    'Exception for not specifying variable';
+  isa_ok $mdb->expect, 'Expect',
+    'lazy builder for Expect object works as expected';
 
-  cmp_ok($mdb->variable_exists("ncsize"), '==', 1,
-         'ncsize should exist as a kernel variable');
+  isnt($mdb->expect->pid, undef, 'There is an mdb PID');
 
-  cmp_ok($mdb->variable_exists("junk"), '==', 0,
-         'junk is a bogus kernel variable');
+  can_ok($mdb, 'quit');
+
+  ok($mdb->quit(), 'Ran quit() method');
+
+  is($mdb->expect->pid, undef, 'mdb PID is now gone');
+}
+
+sub test_kvar_exists {
+  my $test = shift;
+
+  my $mdb = $test->test_mdb;
+  #my $mdb = new_ok("Solaris::mdb" => [ ],
+  #                 "Object constructed correctly");
+
+  isa_ok $mdb->expect, 'Expect',
+    'lazy builder for Expect object works as expected';
+
+  diag("This is the pid: " . $mdb->expect->pid);
+
+  cmp_ok($mdb->expect->pid, ">", 0, 'There is a valid mdb PID');
+
+  can_ok($mdb,'kvar_exists');
+
+  # Pick a well known and *very* unlikely to be eliminated kernel variable
+  cmp_ok($mdb->kvar_exists('bufhwm'), "==", 1, "real kernel variable exists");
+
+  cmp_ok($mdb->kvar_exists('bogus_kvar'), "!=", 1,
+         "bogus kernel variable is not present");
+}
+
+sub test_dcmd_memstat {
+  my $test = shift;
+
+  my $mdb = $test->test_mdb;
+
+  isa_ok $mdb->expect, 'Expect',
+    'lazy builder for Expect object works as expected';
+
+  diag("This is the pid: " . $mdb->expect->pid);
+
+  cmp_ok($mdb->expect->pid, ">", 0, 'There is a valid mdb PID');
+
+  can_ok($mdb,'capture_dcmd');
+
+  like($mdb->capture_dcmd('::memstat'), qr/Kernel/, "::memstat produces output");
+}
+
+sub test_dcmd_memstat_w_time {
+  my $test = shift;
+  my $captured_text;
+
+  my $mdb = $test->test_mdb;
+
+  isa_ok $mdb->expect, 'Expect',
+    'Expect object still exists';
+
+  diag("This is the pid: " . $mdb->expect->pid);
+
+  cmp_ok($mdb->expect->pid, ">", 0, 'There is a valid mdb PID');
+
+  can_ok($mdb,'capture_dcmd');
+
+  $captured_text = 
+    $mdb->capture_dcmd("time::print -d ! sed -e 's/^0t//' ; ::memstat");
+  ok( defined( $captured_text ), 'actually captured SOMETHING from dcmd' ); 
+  like($captured_text,
+       qr/^\d+\n/, "::memstat with time is prefixed with time");
+  like($captured_text,
+       qr/Kernel/, "::memstat with time produces :memstat like output");
+}
+
+
+sub test_dcmd_bogus {
+  my $test = shift;
+
+  my $mdb = $test->test_mdb;
+
+  isa_ok $mdb->expect, 'Expect',
+    'lazy builder for Expect object works as expected';
+
+  diag("This is the pid: " . $mdb->expect->pid);
+
+  cmp_ok($mdb->expect->pid, ">", 0, 'There is a valid mdb PID');
+
+  can_ok($mdb,'capture_dcmd');
+
+  is($mdb->capture_dcmd('::bogus_dcmd'), undef, "::bogus_dcmd fails");
+
+#  cmp_ok($mdb->kvar_exists('bogus_kvar'), "!=", 1,
+#         "bogus kernel variable is not present");
+}
+
+
+sub test_kvar_size {
+  my $test = shift;
+
+  my $mdb = $test->test_mdb;
+
+  can_ok($mdb, 'kvar_size');
+
+  cmp_ok($mdb->kvar_size('bufhwm'),  "==",  4,  'bufhwm size in bytes is correct');
+  is($mdb->kvar_size('bogus_kvar'), undef, 'bogus kernel var size in bytes is correct');
+  # TODO: Test return case of 0
+}
+
+sub test_shutdown {
+  my $test = shift;
+
+  # Do our teardown of the object here
+  my $mdb = $test->test_mdb;
+  undef $mdb;
+
+  # And before exiting...
+  $test->next::method;
 }
 
 1;
